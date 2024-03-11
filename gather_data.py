f7c444355b20ab751957fdaf8e4bb112d11e6acc
jana <jnikolovskaa@gmail.com>
2024-03-11 16:31:54 +0100

    [gather_data.py] refactor

--- gather_data.py
+++ gather_data.py
@@ -1,3 +1,10 @@
+'''
+Script to add NDVI and METEO information to original dataset
+
+Since the dataset we use is ~1035000 records,
+and the weather API has an HOURLY and DAILY limits
+this script was run multiple times in batches in order to process the whole dataset
+'''
 import pandas as pd
 import os
 import warnings
@@ -5,14 +12,16 @@ from utils.utils import add_NDVI_feature, add_weather_info
 warnings.filterwarnings('ignore')
 
 #ADD NDVI
-# data = pd.read_csv(os.path.join('data', 'StorkMigration.csv'))
-# data['timestamp'] = pd.to_datetime(data['timestamp'])
-# data = data[data['timestamp'] >= '2013-03-01']
-# data = add_NDVI_feature(data)
+data = pd.read_csv(os.path.join('data', 'StorkMigration.csv'))
+data['timestamp'] = pd.to_datetime(data['timestamp'])
+data = data[data['timestamp'] >= '2013-03-01']
+data = add_NDVI_feature(data)
 
-data = pd.read_csv('StorkMigrationWithNDVI.csv')
+#ADD METEO DATA
+#data = pd.read_csv('StorkMigrationWithNDVI.csv')
 data['timestamp'] = pd.to_datetime(data['timestamp'])
 print(data.shape)
-data = add_weather_info(data[211779:])
-data.to_csv(os.path.join("data","StorkMigrationWithNDVIWeather-15.csv"))
+data = add_weather_info(data[875671:])
+#data.to_csv(os.path.join("data","FILENAME.csv"))
+data.to_csv(os.path.join("data","StorkMigrationWithNDVIWeather-52.csv"))
 print('Data to csv')
